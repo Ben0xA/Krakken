45142fb8a1e01d03f837e31c6a80a5edd12353e0
Ben0xA <web@ben0xa.com>
2014-05-02 08:02:54 -0500

    Initial Kommit
    The Krakken has been tamed!

---
+++ Krakken.psm1
@@ -0,0 +1,1 @@
+. (Join-Path $PSScriptRoot Unlock-TheKrakken.ps1)
---
+++ Unlock-TheKrakken.ps1
@@ -0,0 +1,75 @@
+function Unlock-TheKrakken{
+	<#    
+		.SYNOPSIS
+  			Disables every NIC on a target machine.
+		.DESCRIPTION
+  			This function will iterate through every NIC on a target machine and disable it.
+    
+		.PARAMETER kill
+  			The remote system on which to start the process.
+	  
+		.EXAMPLE
+  			PS> Unlock-TheKrakken -computer REMOTEPC
+
+		.LINK
+   			https://github.com/ben0xa/krakken/
+		.NOTES
+			AUTHOR: Ben0xA
+  			This function is a utility function for the PoshSec module.
+
+            REQUIRES: PoshSec/Utility-Functions/Invoke-RemoteWmiProcess
+	#>
+    Param(
+        [Parameter(Mandatory=$true,Position=1)]
+        [string]$kill
+    )
+
+    $results = @()
+
+    if($computer -ne "") {
+        $nics = @()
+        # Get a list of all NIC names
+        $ifrslt = Invoke-RemoteWmiProcess $kill "netsh interface show interface"
+        if($ifrslt -ne $null) {
+            $ifstr = $ifrslt.Details
+            $lines = $ifstr -split "\r\n"
+            if($lines -ne $null) {
+                #parse each line from output and extract the name of the interface
+                foreach($line in $lines) {
+                    if(($line -notlike "*Admin State*") -and ($line -notlike "*-------*")) {
+                        $parts = $line -split "   "
+                        if($parts -ne $null) {
+                            $idx = 0
+                            foreach($part in $parts) {
+                                if($part.Trim() -ne "") {
+                                    if($idx -eq 3) {
+                                        $nics += $part.Trim()
+                                    }
+                                    else {
+                                        $idx++
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        if($nics -ne $null) {
+            #disable each NIC found
+            foreach($nic in $nics) {
+                $results += Invoke-RemoteWmiProcess $kill "cmd /c netsh interface set interface name=`"$($nic)`" admin=disabled" -noredirect -nowait
+            }
+        }
+        if($results -ne $null) {
+            $results
+        }
+        else {
+            Write-Output "The Krakken failed. He did not destroy your target."
+        }
+    }
+    else {
+        Write-Output "The Krakken doesn't know who to attack. Please specify a target computer."
+    }
+    
+}
